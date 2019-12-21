27abf292569a5f7750e8b4322fe7373f8fd32d53
Lee Lunn <lee.lunn@dal.ca>
2019-12-21 10:14:50 -0400

    Exploring options to namespace operations

--- relaxed/core.py
+++ relaxed/core.py
@@ -4,6 +4,33 @@ import requests
 from re import sub
 
 
+class User():
+  class __This():
+    __allowed_keys = ['session', 'db']
+
+    def __init__(self, **kwargs):
+      for k in kwargs.keys():
+        if k in self.__allowed_keys:
+          self.__setattr__(k, kwargs.get(k, None))
+
+    def create(self, name, password, **kwargs):
+      docid = f'org.couchdb.user:{name}'
+      userdoc = {'name': name, 'password': password, 'roles': kwargs.get('roles', []), 'type': "user"}
+      return self.db.save_named_doc(uri_segments={'db': '_users', 'docid': docid}, data=userdoc)
+
+  __self = None
+
+  def __init__(self, **kwargs):
+    if not User.__self:
+      User.__self = User.__This(**kwargs)
+    else:
+      for k in kwargs.keys():
+        self.__self.__setattr__(k, kwargs.get(k, getattr(self.__self, k)))
+
+  def __getattr__(self, name):
+    return getattr(self.__self, name)
+
+
 class CouchDB():
     """
     Exposes an interface for interating with a CouchDB server's REST API.
@@ -17,6 +44,7 @@ class CouchDB():
       self.session = Session(**kwargs)
       self.server = Server(session=self.session)
       self.db = Database(session=self.session, **kwargs)
+      self.user = User(session=self.session, db=self.db)
 
     def __enter__(self):
       self._context_manager = True
@@ -24,6 +52,8 @@ class CouchDB():
 
     def __exit__(self, type, value, traceback):
       self._context_manager = False
+
+
 
 
 class CouchError():
---
+++ tests/unit/test_core.py
@@ -0,0 +1,26 @@
+import pytest
+from pytest_httpserver import HTTPServer
+
+from relaxed import AllowedKeys, CouchDB, CouchError, InvalidKeysException
+from relaxed.server import Server
+
+@pytest.fixture
+def httpserver_listen_address():
+    return ("127.0.0.1", 8000)
+
+
+@pytest.fixture(autouse=True)
+def setup():
+  """ setup any state specific to the execution of the given module."""
+  global couch
+  couch = CouchDB(username="test", password="test", host="http://127.0.0.1", port=8000)
+  yield
+
+
+def test_creating_a_valid_user(httpserver: HTTPServer):
+  expected_json = {"ok": True, "id": "org.couchdb.user:test", "rev": "1-e0ebfb84005b920488fc7a8cc5470cc0"}
+
+  httpserver.expect_request("/_users/org.couchdb.user:test",  method="PUT").respond_with_json(expected_json)
+  response = couch.user.create(name='test', password='test')
+
+  assert response == expected_json
