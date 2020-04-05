caa1fbd6430f5ab84b1a996782cc01cbc918fb18
Lee Lunn <lee@torusoft.com>
2020-04-05 16:46:49 -0300

    [BREAKING] code kwarg changed to status_code

--- couchapy/error.py
+++ couchapy/error.py
@@ -3,4 +3,4 @@ class CouchError():
     def __init__(self, **kwargs):
         self.error = kwargs.get('error', None)
         self.reason = kwargs.get('reason', None)
-        self.status_code = kwargs.get('code', None)
+        self.status_code = kwargs.get('status_code', None)
