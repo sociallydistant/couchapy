9bdd18976e6189f10d522c06b571a3091a9b9bb8
Lee Lunn <lee@torusoft.com>
2020-04-05 10:58:04 -0300

    Output code coverage summary to stdout

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,1 +1,1 @@
-python3 -m pytest --cov-report html:docs/coverage/html --cov-config=.coveragerc --cov=couchapy tests/
+python3 -m pytest --cov-report term --cov-report html:docs/coverage/html --cov-config=.coveragerc --cov=couchapy tests/
