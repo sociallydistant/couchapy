fe515c9e6fce86efb61b2f00c3e0ced07cb6664a
Lee Lunn <lee@torusoft.com>
2020-04-05 15:24:36 -0300

    Updates docstring and constructor

--- couchapy/session.py
+++ couchapy/session.py
@@ -13,10 +13,7 @@ class Session():
     """
     Interacts with a CouchDB server's REST API for session management.
 
-    Examples:
-    couchDB = Session(address="https://somehost.com", port="6984", username="admin", password="super_secure", address="https://somehost.com", port="6984")
-    couchDB = Session(address="https://somehost.com", port="6984", auth_token="bGVlLmx1bm5AZGFsLmNhOjVERTQ4RjhCOq-IvL4mhVVUFn4k5H1bIYiggf3X")
-    couchDB = Session(address="https://somehost.com", port="6984", username="admin", password="super_secure", keep_alive=290)
+    This class is not intended to be instanced directly.
 
     Attributes:
 
@@ -24,21 +21,20 @@ class Session():
     If provided, authentication using AuthSession will be attempted. AuthSession is set/updated
     with successful authentication when connecting with username and password. (Default: None)
 
-    :param bool basic_auth: Sets authentication method to the CouchDB server to Basic. If basic authentication is used, auto_connect has no effect. (Default: False)
-
-    :param dict custom_headers: Dictionary of custom headers to add to each request to the CouchDB server. (Default: None)
+    :param CouchDB parent   CouchDB instance.
+    :param str  auth_token  Most recent session token retreived from the CouchDB Server
+                            specified by parent
+    :param bool basic_auth  Sets authentication method to the CouchDB server to Basic. If basic authentication is used, auto_connect has no effect. (Default: False)
     """
-
     def __init__(self, parent, **kwargs):
 
         self.auth_token = kwargs.get('auth_token', None)
         self.basic_auth = kwargs.get('basic_auth', False)  # TODO: implement basic auth
 
+        # reference to the parent CouchDB instance.
+        # Primarily required for the endpoint decorator to access
+        # server configuration such as host and port
         self.parent = parent
-
-
-        # reference to this object is required for the CouchDBDecorators.endpoint to be able to update the auth token
-        # self.session = self
 
     # def __del__(self):
     #     if (self._keep_alive > 0):
